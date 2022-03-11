31bc33535520dd20dc678c007fa40f9b023763f6
vitoriajaco <97560091+vitoriajaco@users.noreply.github.com>
2022-03-10 21:06:37 -0300

    Criando bolinha e adicionando variaveis

---
+++ jogopong.js
@@ -0,0 +1,17 @@
+let xBolinha = 300
+let yBolinha = 200
+let diametro = 15
+width = 600
+height = 400
+
+
+let velocidadexBolinha = 5
+let velocidadeyBolinha = 6
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(0);
+  circle(xBolinha, yBolinha,diametro);
